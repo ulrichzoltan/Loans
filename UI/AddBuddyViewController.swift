7f10d588000e517ffce8fab84f0bf6715929ba4f
Istvan Szekely <istvan.szekely@iquestgroup.com>
2016-08-26 14:43:39 +0300

    Added AddBuddy VC

---
+++ UI/AddBuddyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Created by Istvan Szekely on 26/08/16.
+//  Copyright © 2016 iQuest Technologies. All rights reserved.
+//
+
+import UIKit
+
+class AddBuddyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
