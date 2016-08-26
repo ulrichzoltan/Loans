516d04a445de91ff9d7969d08231fe0be0f1d3e1
Istvan Szekely <istvan.szekely@iquestgroup.com>
2016-08-26 18:05:27 +0300

    started implementing the body list tableview

--- BuddyListViewController.swift
+++ BuddyListViewController.swift
@@ -5,39 +5,62 @@
 
 import UIKit
 
-class BuddyListViewController: UIViewController {
-
+class BuddyListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var buddyTableView: UITableView!
-
+    var buddyList = []
     required init?(coder aDecoder: NSCoder) {
-
+        
         transactionService = TransactionService(withUser: user!)
-
+        
         super.init(coder: aDecoder)
-
+        
         transactionService!.delegate = self
     }
     
-
+    
     override func viewDidLoad() {
-
         super.viewDidLoad()
+        
+        self.buddyTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        buddyTableView.delegate = self
+        buddyTableView.dataSource = self
+        buddyList = (appDelegate?.loans?.transactions)!
+        
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.buddyList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = self.buddyTableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
+        cell.textLabel?.text = (self.buddyList[indexPath.row] as! Transaction).userId
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You tapped cell number \(indexPath.row).")
     }
 }
 
 extension BuddyListViewController: TransactionServiceDelegate {
-
+    
     func didReceive(transaction transaction: Transaction) {
-
+        
         print("Received transaction: \(transaction)")
         let response = Response(success: true, message: "Thanks for the loan.")
         appDelegate?.loans?.update(transaction)
-
+        
         transactionService!.send(response, to: transaction.userId) { error in
             print("Sent response.")
         }
     }
-
+    
     func didReceive(response response: Response) {
     }
 }
