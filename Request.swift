e322012d3b445d3b1e50bba7f13dbdb32d245536
alparkatona <alpar.katona@gmail.com>
2016-08-26 11:28:52 +0300

    added new classes Request & Response

---
+++ Request.swift
@@ -0,0 +1,29 @@
+//
+//  Request.swift
+//  Loans
+//
+//  Created by alpar on 26/08/16.
+//  Copyright © 2016 iQuest Technologies. All rights reserved.
+//
+
+import Foundation
+import Transaction
+
+class Request: NSObject {
+    
+    let recipientId: String
+    let transaction: Transaction
+    
+    init(withRecipientId recipientId: String,
+                         transaction transaction: Transaction)
+    {
+        self.recipientId = recipientId
+        self.transaction = transaction
+        
+        super.init()
+    }
+}
+
+
+
+
--- Transaction.swift
+++ Transaction.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+class Transaction: NSObject, NSCoding {
+    
+    private struct Keys {
+        static let userId = "userId"
+        static let transaction = "transaction"
+    }
+    
+    let userId: String
+    let value: Int
+    let date: NSTimeInterval
+    let description: String
+    
+    init(withUserId userId: String,
+                    value value: Int,
+                          date date: NSTimeInterval,
+                               description description: String) {
+        
+        self.userId = userId
+        self.value = value
+        self.date = date
+        self.description = description
+        
+        super.init()
+    }
+    
+//    class func savedTransaction() -> Transaction? {
+//        
+//        return NSUserDefaults.standardUserDefaults().objectForKey(Keys.transaction) as? Transaction
+//    }
+//    
+//    func save() {
+//        
+//        let data = NSKeyedArchiver.archivedDataWithRootObject(self)
+//        NSUserDefaults.standardUserDefaults().setObject(data, forKey: Keys.transaction)
+//        NSUserDefaults.standardUserDefaults().synchronize()
+//    }
+//    
+//    @objc required init?(coder aDecoder: NSCoder) {
+//        
+//        // I know of no way we can make a decoder provide invalid input
+//        // in order to test the failure case.
+//        
+//        id = aDecoder.decodeObjectForKey(Keys.userId) as! String
+//    }
+//    
+//    @objc func encodeWithCoder(aCoder: NSCoder) {
+//        
+//        aCoder.encodeObject(userId, forKey: Keys.userId)
+//    }
+}
+
