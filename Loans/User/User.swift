1c9f1acc36f267c5fc3d5100108e041ba744b73b
Zoltan Ulrich <zoltan.ulrich@iquestgroup.com>
2016-08-26 09:37:26 +0300

    Added User.

---
+++ Loans/User/User.swift
@@ -0,0 +1,49 @@
+//
+//  User.swift
+//  
+//
+//  Created by Zoltan Ulrich on 26.08.2016.
+//
+//
+
+import Foundation
+
+class User: NSObject, NSCoding {
+
+    private struct Keys {
+        static let id = "id"
+        static let displayName = "displayName"
+
+        static let user = "user"
+    }
+
+    let id: String
+    let displayName: String
+
+    class func savedUser() -> User? {
+
+        return NSUserDefaults.standardUserDefaults().objectForKey(Keys.user) as? User
+    }
+
+    func save() {
+
+        let data = NSKeyedArchiver.archivedDataWithRootObject(self)
+        NSUserDefaults.standardUserDefaults().setObject(data, forKey: Keys.user)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+
+    @objc required init?(coder aDecoder: NSCoder) {
+
+        // I know of no way we can make a decoder provide invalid input
+        // in order to test the failure case.
+
+        id = aDecoder.decodeObjectForKey(Keys.id) as! String
+        displayName = aDecoder.decodeObjectForKey(Keys.displayName) as! String
+    }
+
+    @objc func encodeWithCoder(aCoder: NSCoder) {
+
+        aCoder.encodeObject(id, forKey: Keys.id)
+        aCoder.encodeObject(displayName, forKey: Keys.displayName)
+    }
+}
